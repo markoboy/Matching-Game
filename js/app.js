dbad7f217a598d4e1aee37bd40ff9a9bfeb0768e
Athanasios Markou <thanasismarko@gmail.com>
2018-03-29 08:34:38 +0100

    feat: Add card obj to handle card actions

--- js/app.js
+++ js/app.js
@@ -50,6 +50,43 @@ const game = {
     }
 }
 
+/** @description - Create card obj to handle card actions.
+  * @param {array} isOpened - List opened cards to match.
+  * @param {integer} matched - Counts the opened cards.
+  * @param {function} open - Function to open cards.
+  */
+const card = {
+    'isOpened': [],
+    'matched' : 0,
+    'open'    : function(e) {
+        // Check if the target is a card and game is running.
+        if (e.target.nodeName === 'LI' && e.target.className === 'card' && game.isActive) {
+            // Store clicked card in a variable.
+            let picked = e.target;
+            // Check how many cards are opened.
+            let opened = card.isOpened.length;
+            switch (opened) {
+                case 0:
+                    // Display card.
+                    picked.classList.add('open', 'show');
+                    // Store picked card.
+                    card.isOpened.push(picked);
+                    break;
+                case 1:
+                    card.check(picked, card.isOpened[0])
+                    console.log('Check if cards are the same.');
+            }
+        } else if (!game.isActive) {
+            game.start();
+            console.log('Game started');
+        }
+    },
+    'check'   : function(newCard, oldCard) {
+        // Display card.
+        newCard.classList.add('open', 'show');
+    }
+}
+
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -60,8 +97,4 @@ const game = {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
-
-/*
- * Add DOMContentLoaded event to document for a fresh start.
- */
-// document.addEventListener('DOMContentLoaded', newGame);
+deck.addEventListener('click', card.open);
